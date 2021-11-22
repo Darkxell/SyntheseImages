06cfd40afca4c30d16a225d77b016a027ffc7ed5
CANDELA Nicolas <darkxell.mc@gmail.com>
2021-11-22 17:36:22 +0100

    Added red shader

--- opengl/opengl.cpp
+++ opengl/opengl.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+	
+	// Shader compile
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	//Error handling
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (!result) {
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile shader : " << std::endl;
+		std::cout << message << std::endl;
+
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string fragmentShader) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs); glAttachShader(program, fs);
+	
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs); glDeleteShader(fs);
+
+	return program;
+}
 
 int main(void)
 {
@@ -48,6 +92,26 @@ int main(void)
 	// Set vertex attributes
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 	glEnableVertexAttribArray(0);
+
+	// Set the rendering shader
+	std::string vrtshader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;"
+		"\n"
+		"void main(){\n"
+		"	gl_Position = position;\n"
+		"}\n";
+	std::string frgshader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;"
+		"\n"
+		"void main(){\n"
+		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"}\n";
+	unsigned int shader = CreateShader(vrtshader, frgshader);
+	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
