80ca48402bb59ed53026c84a3aadb8723a7fd481
CANDELA Nicolas <darkxell.mc@gmail.com>
2021-11-22 14:47:03 +0100

    Added glew

--- opengl/opengl.cpp
+++ opengl/opengl.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
 	// https://www.glfw.org/
 	GLFWwindow* window;
+	
+	/* Initialize the libraries and creates an openGL context.*/
+	{
+		if (!glfwInit())
+			return -1;
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
+		/* Create a windowed mode window and its OpenGL context */
+		window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
+		if (!window) {
+			glfwTerminate();
+			return -1;
+		}
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
-	if (!window) {
-		glfwTerminate();
-		return -1;
+		/* Make the window's context current */
+		glfwMakeContextCurrent(window);
+
+		if (glewInit() != GLEW_OK) {
+			std::cout << "Glew couldn't initialize." << std::endl;
+			return -1;
+		}
+		
+		std::cout << "Context ready, with GLEW and GLFW initialised." << std::endl;
+		std::cout << "Opengl version : " << glGetString(GL_VERSION)  << std::endl;
 	}
-
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-
+	
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
 		/* Render here */
